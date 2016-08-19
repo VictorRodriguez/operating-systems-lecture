7c538b0b18adf5d20749ed79a025d2495fe9bf0c
VictorRodriguez <vm.rod25@gmail.com>
2016-08-19 05:10:12 -0500

    Add sys calls examples
    Signed-off-by: VictorRodriguez <vm.rod25@gmail.com>

---
+++ labs/02/chmod.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/syscall.h>
+#include <errno.h>
+
+
+int main (){
+
+    int rc;
+
+    rc = chmod("/etc/passwd", 0444);
+
+    if (rc == -1){
+        fprintf(stderr, "chmod failed, errno = %d\n", errno);
+    }
+}
