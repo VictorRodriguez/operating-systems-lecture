d9d6933a276f8dfcc36f92e0440a3fac857e08e9
Juan Manuel Romero Guardado <jmromerog511@gmail.com>
2016-10-03 14:29:19 -0500

    -a

--- labs/05/round_robin.py
+++ labs/05/round_robin.py
@@ -1,49 +1,89 @@
 
-#
-# Round Robin calculator
-#
-# Simulate the Round Robin mechanism based on a queue of processes 
-# Print the Avg turn around time
-# 
-# Please improve so it can be used since command line and plotted 
-#
-# process = [] might be a random list of elapsed times 
-#
-# Experiment with this and make 3D graphs for:
-#   
-#   avg_turn_time vs quantum vs max process size
-#
-#   context_switch vs  quantum vs max process size
-#
+# autor: Juan Manuel
 
-quantum = 4
-context_switch = 2
-execution_time = 0
-avg_turnaround_tm = 0.0
-context_switchs = 0
+# Simulate the Round Robin mechanism based on a queue of processes
+# Graph the behavior of avg_turnaround_tms and context_switches 
+# of a queue of processes with diferent quantum values
+#
+# process = [] might be a random list of elapsed times
+
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+import matplotlib.pyplot as plt
+
+def printInfo():
+    print ("Quantums") 
+    print quantums
+    print ("Avg Turnaround times:") 
+    print avg_turnaround_tms
+    print ("Context Switches") 
+    print context_switches
+    print("Initial processes")
+    print processes
+
+quantum = input("Enter the initial quantum: ")
+quantum_max = input("Enter the final quantum: ")
+quantums = []
+
+context_switches = []
+avg_turnaround_tms = []
+max_process_sizes = []
 
 processes =[2,3,4,12,8,5,6,1]
 num_processes = len(processes)
 
-for process in processes:
-    if process <= quantum:
-        print("process executed")
-        execution_time+=process
-        print("execution time " + str(execution_time))
-    else:
-        print("process interrupted")
-        remain_time = process - quantum
-        context_switchs+=1
-        print("time process executed: "+ str(quantum))
-        execution_time+=quantum
-        execution_time+=context_switch
-        print("execution time " + str(execution_time))
-        processes.append(remain_time)
+while quantum <= quantum_max:
+    
+    processes =[2,3,4,12,8,5,6,1]
+    max_process_size = 0
+    context_switch = 2
+    context_switchs = 0
+    execution_time = 0
+    avg_turnaround_tm = 0.0
 
-    print processes
-    print
+    i = 0
+    quantums.append(quantum)
 
-avg_turnaround_tm = float(execution_time)/float(num_processes)
-print ("Number of processes : " + str(num_processes))
-print ("Number of context switch : " + str(context_switchs))
-print ("Avg turn around time : " + str(avg_turnaround_tm))
+    while i < len(processes):
+
+        if max_process_size < processes[i]:
+            max_process_size = processes[i]
+            print max_process_size
+
+        if processes[i] <= quantum:
+            print("process executed")
+            execution_time += processes[i]
+            print("execution time " + str(execution_time))
+        else:
+            print("process interrupted")
+            remain_time = processes[i] - quantum
+            context_switchs += 1
+            print("time process executed: "+ str(quantum))
+            execution_time+=quantum
+            execution_time+=context_switch
+            print("execution time " + str(execution_time))
+            processes.append(remain_time)
+
+        print processes
+        print
+        i += 1
+
+    avg_turnaround_tm = float(execution_time)/float(num_processes)
+    avg_turnaround_tms.append(avg_turnaround_tm)
+    max_process_sizes.append(max_process_size)
+    context_switches.append(context_switchs)
+    quantum += 1
+
+printInfo()
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+z = max_process_sizes
+y = avg_turnaround_tms
+# y = context_switches
+x = quantums
+
+ax.plot_trisurf(x, y, z, cmap=cm.jet, linewidth=0.2)
+
+plt.show()
