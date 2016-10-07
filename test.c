96876c2b12164be8a23e4bfa62f2498187698532
VictorRodriguez <vm.rod25@gmail.com>
2016-10-07 08:14:51 -0500

    test for fmv
    Signed-off-by: VictorRodriguez <vm.rod25@gmail.com>

---
+++ test.c
@@ -0,0 +1,12 @@
+#define MAX 1000000
+int a[256], b[256], c[256];
+int main () {
+    int i,j;
+    for (j=0;j<MAX;j++){
+               for (i=0; i<256; i++){
+                                  a[i] = b[i] + c[i];
+                                       }
+                   return 0;
+    }
+}
+
