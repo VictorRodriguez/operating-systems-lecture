77b5097501ff85937c924b9d5714f1c9df74f4db
VictorRodriguez <vm.rod25@gmail.com>
2018-02-09 08:33:37 -0600

    didactic for next class
    Signed-off-by: VictorRodriguez <vm.rod25@gmail.com>

--- labs/04/mutex-thread.c
+++ labs/04/mutex-thread.c
@@ -15,16 +15,17 @@ pthread_mutex_t lock;
 
 void* doSomeThing(void *arg)
 {
-    pthread_mutex_lock(&lock);
+//    pthread_mutex_lock(&lock);
     unsigned long i = 0;
     
     counter += 1;
     printf("\n Job %d started\n", counter);
 
     for(i=0; i<(0xFFFFFFFF);i++){
-        printf("\n Job %d finished\n", counter);
     }
-    pthread_mutex_unlock(&lock);
+
+    printf("\n Job %d finished\n", counter);
+//    pthread_mutex_unlock(&lock);
 
     return NULL;
 }
