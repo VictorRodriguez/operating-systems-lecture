fadc6df7fe17f5de500b03e62bdcc124ee594ea5
Victor Rodriguez <victor.rodriguez.bahena@intel.com>
2018-02-21 23:25:52 +0000

    Improve to get exteral value of iterations
    Signed-off-by: Victor Rodriguez <victor.rodriguez.bahena@intel.com>

--- labs/gcc/simple-math-bench.c
+++ labs/gcc/simple-math-bench.c
@@ -1,14 +1,37 @@
-#define MAX 1000000
-int a[256], b[256], c[256];
-int main () {
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void foo(long iter){
     int i,j,k =0;
-    for (j=0;j<MAX;j++){
-        for (j=0;j<MAX;j++){
-            for (i=0; i<256; i++){
+    int a[256], b[256], c[256];
+
+    printf("Running foo\n");
+    printf("iterations = %lu \n",(iter*iter));
+    printf(" math = a[i] = b[i] + c[i]\n");
+
+    for (j=0;j<iter;j++)
+        for (j=0;j<iter;j++)
+            for (i=0; i<256; i++)
                 a[i] = b[i] + c[i];
+}
+
+int main (int argc , char **argv){
+
+    int c;
+    long iter;
+
+    while ((c = getopt(argc,argv,"i:")) != -1 )
+        switch (c){
+            case 'i':
+                iter = atol(optarg);
+                break;
+            default:
+                abort();
         }
-    }
+
+    foo(iter);
     return 0;
-    }
 }
 
