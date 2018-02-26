f7be6851fdff985645e21430b35c264916a4ea85
Victor Rodriguez <victor.rodriguez.bahena@intel.com>
2018-02-26 21:17:58 +0000

    improve python script for py3 add optargs
    Signed-off-by: Victor Rodriguez <victor.rodriguez.bahena@intel.com>

--- labs/05/round_robin_profesor_version.py
+++ labs/05/round_robin_profesor_version.py
@@ -5,7 +5,7 @@
 # Simulate the Round Robin mechanism based on a queue of processes 
 # Print the Avg turn around time
 # 
-# Please improve so it can be used since command line and plotted 
+# It can be used from command line
 #
 # process = [] might be a random list of elapsed times 
 #
@@ -13,37 +13,58 @@
 #   
 #   avg_turn_time vs quantum vs max process size
 #
-#   context_switch vs  quantum vs max process size
+#   context_switch vs  quantum vs context_switchs
 #
 
-quantum = 4
-context_switch = 2
-execution_time = 0
-avg_turnaround_tm = 0.0
-context_switchs = 0
+import sys, getopt
 
-processes =[2,3,4,12,8,5,6,1]
-num_processes = len(processes)
 
-for process in processes:
-    if process <= quantum:
-        print("process executed")
-        execution_time+=process
-        print("execution time " + str(execution_time))
-    else:
-        print("process interrupted")
-        remain_time = process - quantum
-        context_switchs+=1
-        print("time process executed: "+ str(quantum))
-        execution_time+=quantum
-        execution_time+=context_switch
-        print("execution time " + str(execution_time))
-        processes.append(remain_time)
 
-    print processes
-    print
+def main(argv):
 
-avg_turnaround_tm = float(execution_time)/float(num_processes)
-print ("Number of processes : " + str(num_processes))
-print ("Number of context switch : " + str(context_switchs))
-print ("Avg turn around time : " + str(avg_turnaround_tm))
+    quantum = 4
+    context_switch = 2
+    execution_time = 0
+    avg_turnaround_tm = 0.0
+    context_switchs = 0
+
+    processes =[2,3,4,12,8,5,6,1]
+    num_processes = len(processes)
+
+    try:
+        opts, args = getopt.getopt(argv,"hq:",["quantum_time="])
+    except getopt.GetoptError:
+        print("round_robin.py -q <quantum time>")
+        sys.exit(-1)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ("test.py -q <quantum time>")
+            sys.exit()
+        elif opt in ("-q", "--quantum_time"):
+            quantum = int(arg)
+
+    for process in processes:
+        if process <= quantum:
+            print("process executed")
+            execution_time+=process
+            print("execution time " + str(execution_time))
+        else:
+            print("process interrupted")
+            remain_time = process - quantum
+            context_switchs+=1
+            print("time process executed: "+ str(quantum))
+            execution_time+=quantum
+            execution_time+=context_switch
+            print("execution time " + str(execution_time))
+            processes.append(remain_time)
+
+        print(processes)
+        print()
+
+    avg_turnaround_tm = float(execution_time)/float(num_processes)
+    print ("Number of processes : " + str(num_processes))
+    print ("Number of context switch : " + str(context_switchs))
+    print ("Avg turn around time : " + str(avg_turnaround_tm))
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
