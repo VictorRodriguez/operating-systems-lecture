a83c8f0201fcd3601588e8e0a71628a86eafdc4c
Victor Rodriguez <victor.rodriguez.bahena@intel.com>
2018-03-10 15:01:09 +0000

    add fma using intel instructions
    Signed-off-by: Victor Rodriguez <victor.rodriguez.bahena@intel.com>

--- labs/gcc/fma_optimized.c
+++ labs/gcc/fma_optimized.c
@@ -18,27 +18,25 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <immintrin.h>
 
-void multiply_and_add(const float* a, const float* b, const float* c, float* d) {  
 
-  for(int i=0; i<8; i++) {
-    d[i] = a[i] * b[i];
-    d[i] = d[i] + c[i];
-  }
+__m256 multiply_and_add(__m256 a, __m256 b, __m256 c) {
+
+  return _mm256_fmadd_ps(a, b, c);
 }
-
 
 int main ( int argc, char *argv[] ){
 
-    float a[8] = {1,1,1,1,1,1,1,1};
-    float b[8] = {2,2,2,2,2,2,2,2};
-    float c[8] = {3,3,3,3,3,3,3,3};
-    float d[8];
-    
-    multiply_and_add(a,b,c,d);
+    __m256 a = _mm256_set_ps(1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0);
+    __m256 b = _mm256_set_ps(2.0,2.0,2.0,2.0,2.0,2.0,2.0,2.0);
+    __m256 c = _mm256_set_ps(3.0,3.0,3.0,3.0,3.0,3.0,3.0,3.0);
 
-    for (int i=0; i< sizeof(d)/sizeof(float) ; i++){
-        printf("%f\n",d[i]);
-    }
+    __m256 result = multiply_and_add(a,b,c);
+
+    float* f = (float*)&result;
+    printf("%f %f %f %f %f %f %f %f\n",
+            f[0], f[1], f[2], f[3], f[4], f[5], f[6], f[7]);
+
     return EXIT_SUCCESS;
 }
