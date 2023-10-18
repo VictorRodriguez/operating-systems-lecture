59c80968171e94de1a5cdbbb02c055171b43c147
Erick Oseguera Salinas <133702336+ErickOsegueraS@users.noreply.github.com>
2023-10-17 21:56:38 -0600

    Update A01068271-homework-03.C

--- labs/03/A01068271-homework-03.C
+++ labs/03/A01068271-homework-03.C
@@ -11,9 +11,10 @@
 #include <dirent.h>
 #include <fcntl.h>
 #include <limits.h>
+#include <errno>
 
 #define MAX_COMMAND_LENGTH 100
-#define MAX_INPUT_SIZE 1024
+#define MAX_INPUT_LENGTH 1024
 
 void cus_ls() {
     struct dirent *entry;
@@ -39,42 +40,53 @@ void cus_pwd() {
 }
 
 int main() {
-    char input[MAX_INPUT_SIZE];
+    char input[MAX_INPUT_LENGTH];
+    char *args[MAX_INPUT_LENGTH];
+    int status;
+
     while (1) {
-        printf("MiTerminal> ");
-        fgets(input, MAX_INPUT_SIZE, stdin);
+        printf("osh> "); // Muestra el indicador "osh>"
+        fgets(input, MAX_INPUT_LENGTH, stdin);
 
-        input[strcspn(input, "\n")] = '\0';
+        // Quitamos el espacio después del comando
+        input[strcspn(input, "\n")] = 0;
 
-        if (strcmp(input, "exit") == 0) {
-            printf("Saliendo de MiTerminal...\n");
-            break;
+        // Convertimos los input en tokens para poder pasarlos a execvp
+        char *token = strtok(input, " ");
+        int i = 0;
+        // Separamos los argumentos
+        while (token != NULL) {
+            args[i] = token;
+            token = strtok(NULL, " ");
+            i++;
         }
+        args[i] = NULL;
 
-        pid_t child_pid = fork();
-        if (child_pid == 0) {
-            // Proceso hijo
-            if (strstr(input, "cat ") == input) {
-                char *archOrigen = input + 4;
-                char *argv[] = {"cat", archOrigen, NULL};
-                execvp("cat", argv);
-                perror("Error al ejecutar el comando");
-                exit(1);
-            } else if (strcmp(input, "ls") == 0) {
-                cus_ls();
-            } else if (strcmp(input, "pwd") == 0) {
-                cus_pwd();
-            } else {
-                execl("/bin/sh", "/bin/sh", "-c", input, (char *)0);
-                perror("Error al ejecutar el comando");
-                exit(1);
-            }
-        } else if (child_pid < 0) {
-            perror("Error al realizar el fork");
+        if (strcmp(args[0], "exit") == 0) {
+            printf("Saliendo del programa...\n");
+            break;
+        } else if (strcmp(args[0], "ls") == 0) {
+            printf("osh> ls\n");
+            cus_ls();
+        } else if (strcmp(args[0], "pwd") == 0) {
+            printf("osh> pwd\n");
+            cus_pwd();
         } else {
-            // Proceso padre
-            int status;
-            waitpid(child_pid, &status, 0);
+            // Hacemos un fork del hijo
+            pid_t pid = fork();
+            if (pid == 0) {
+                // Child process
+                execvp(args[0], args);
+                printf("No se encontro el comando\n");
+                exit(1);
+            } else if (pid < 0) {
+                // Fork fallido si el ID es negativo
+                printf("Fork failed\n");
+                exit(1);
+            } else {
+                // Proceso padre
+                wait(NULL);
+            }
         }
     }
 
