e61fda914fd504bc7518b8d641b1310d4b3c2914
EdwinGitPro <a01637064@tec.mx>
2023-10-17 21:58:22 -0600

    a01637064-homework-01
    Signed-off-by: EdwinGitPro <a01637064@tec.mx>

--- labs/01/hello.c
+++ labs/01/hello.c
@@ -4,6 +4,6 @@
 
 int main(void) {
     printf("Hello World\n");
-    printf("My name is Christopher Salvador Márquez Álvarez \n");
+    printf("My name is Edwin Iniguez Moncada \n");
     return 0; 
 }
