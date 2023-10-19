3788bf48bcf02395bc95d44f86746f6641c65b8b
Noé Benjamín Rosales Medina <145573317+NoeRosales22@users.noreply.github.com>
2023-10-19 11:24:36 -0600

    Update myTerminal.c

--- labs/03/myTerminal.c
+++ labs/03/myTerminal.c
@@ -1,5 +1,8 @@
 /*
-
+Noé Benjamín Rosales Medina
+A00227290
+Tarea 1.5 Simple Terminal (fork/exec)
+19/10/2023
 */
 
 #include <sys/types.h>
@@ -9,36 +12,46 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <string.h>
+
 int main(){
-        char instruction[100];
+        // Variables declaration
+        char command[100];
         char stop[] = "stop";
         char ls[] = "ls";
         char date[] = "date";
         char pwd[] = "pwd";
         pid_t pid;
-        while(strcmp(instruction, stop) != 0){
+
+        while(strcmp(command, stop) != 0){
                 printf("nbrm> ");
-                scanf("%s", instruction);
-                if(strcmp(instruction, date) == 0 || strcmp(instruction, ls) == 0 || strcmp(instruction, pwd) == 0){
+                scanf("%s", command);
+                if(strcmp(command, date) == 0 || strcmp(command, ls) == 0 || strcmp(command, pwd) == 0){
+                        // Fork a child process
                         pid = fork();
                         if(pid < 0){
+                                // Error ocurred
                                 fprintf(stderr, "Fork Failed");
                                 exit(-1);
                         }
-                        else if (pid == 0 && strcmp(instruction, ls) == 0){
+                        else if (pid == 0 && strcmp(command, ls) == 0){
+                                // Child process for ls
                                 execlp("/bin/ls","ls",NULL);
                         }
-                        else if (pid == 0 && strcmp(instruction, date) == 0){
+                        else if (pid == 0 && strcmp(command, date) == 0){
+                                // Child process for date
                                 execlp("/bin/date","date",NULL);
                         }
-                        else if (pid == 0 && strcmp(instruction, pwd) == 0){
+                        else if (pid == 0 && strcmp(command, pwd) == 0){
+                                // Child process for pwd
                                 execlp("/bin/pwd","pwd",NULL);
                         }
                         else{
+                                // Parent waits for the child to complete its process
                                 wait(NULL);
                         }
                 }
-                else if (strcmp(instruction, stop) != 0){
+                else if (strcmp(command, stop) != 0){
+                        // Any other command
                         printf("nbrm> Wrong command, stop to exit the terminal\n");
                 }
         }
