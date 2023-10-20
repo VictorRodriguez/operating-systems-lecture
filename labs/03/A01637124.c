e805ff93a192f3e3c850ef80a92c741b7c780938
ZAnaKarenZ <117191903+ZAnaKarenZ@users.noreply.github.com>
2023-10-19 20:52:17 -0600

    A01637124_Tarea 1

---
+++ labs/03/A01637124.c
@@ -0,0 +1,82 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+int main() {
+  pid_t pid;
+  int opc;
+  do{
+    printf("¡Hola! :D Te doy la bienvenida.\n");
+    printf("¿Que te gustaria hacer?\n");
+    printf("Menu: \n");
+    printf("\t1. Salir\n");
+    printf("\t2. date\n");
+    printf("\t3. pwd\n");
+    printf("\t4. ls\n");
+    printf("¿Cual opcion eliges?\n");
+    scanf("%d", &opc);
+    switch (opc){
+      case 1:
+        printf("Saliendo...");
+        break;
+
+      case 2:
+
+	    if (pid < 0) {                                         //Detener en caso de error
+		    fprintf(stderr, "Error: No se pudo realizar la operacion Fork.");
+		    exit (-1) ;
+	    }
+
+	    else if (pid == 0) {                                   //Hijo realiza operación date
+		    execlp("/bin/date","date",NULL);
+	    }
+
+	    else {                                                 //Padre espera a que termine el hijo
+		    wait(NULL);
+	    }
+        break;
+
+      case 3:
+	    pid = fork();                                          //Fork
+
+	    if (pid < 0) {                                         //Detener en caso de error
+		    fprintf(stderr, "Error: No se pudo realizar la operacion Fork.");
+		    exit (-1) ;
+	    }
+
+	    else if (pid == 0) {                                   //Hijo realiza operación pwd
+		    execlp("/bin/pwd","pwd",NULL);
+	    }
+
+	    else {                                                 //Padre espera a que termine el hijo
+		    wait(NULL);
+	    }
+        break;
+
+      case 4:
+	    pid = fork();                                          //Fork
+
+	    if (pid < 0) {                                         //Detener en caso de error
+		    fprintf(stderr, "Error: No se pudo realizar la operacion Fork.");
+		    exit (-1) ;
+	    }
+
+	    else if (pid == 0) {                                   //Hijo realiza operación ls
+		    execlp("/bin/ls","ls",NULL);
+	    }
+
+	    else {                                                 //Padre espera a que termine el hijo
+		    wait(NULL);
+	    }
+        break;
+
+      default:
+        break;
+    }
+  } while(opc != 1);
+
+    return 0;
+}
